52ba4256f3ae4c78a4638d662ae68f640ee4b1d0
zyronon <zyronon@163.com>
2022-12-27 00:16:32 +0800

    save

--- js特效/裁剪/assets/js/qunit.js
+++ js特效/裁剪/assets/js/qunit.js
@@ -166,7 +166,7 @@ config.modules.push( config.currentModule );
 			current = params[ i ].split( "=" );
 			current[ 0 ] = decodeURIComponent( current[ 0 ] );
 
-			// allow just a key to turn on a flag, e.g., test.html?noglobals
+			// allow just a key to turn on a flag, e.g., 自己写的，可以任意放大.html?noglobals
 			current[ 1 ] = current[ 1 ] ? decodeURIComponent( current[ 1 ] ) : true;
 			if ( urlParams[ current[ 0 ] ] ) {
 				urlParams[ current[ 0 ] ] = [].concat( urlParams[ current[ 0 ] ], current[ 1 ] );
