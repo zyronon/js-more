9626b94a14db37075993f5b08db939023a618bcf
wangnianchao <wangnianchao@lerjin.com>
2022-12-26 18:40:40 +0800

    a

---
+++ sol/3-BlindAuction.sol
@@ -0,0 +1,142 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+contract BlindAuction {
+    struct Bid {
+        bytes32 blindedBid;
+        uint256 deposit;
+    }
+
+    address payable public beneficiary;
+    uint256 public biddingEnd;
+    uint256 public revealEnd;
+    bool public ended;
+
+    mapping(address => Bid[]) public bids;
+
+    address public highestBidder;
+    uint256 public highestBid;
+
+    // 可以取回的之前的出价
+    mapping(address => uint256) pendingReturns;
+
+    event AuctionEnded(address winner, uint256 highestBid);
+
+    // Errors that describe failures.
+
+    /// The function has been called too early.
+    /// Try again at `time`.
+    error TooEarly(uint256 time);
+    /// The function has been called too late.
+    /// It cannot be called after `time`.
+    error TooLate(uint256 time);
+    /// The function auctionEnd has already been called.
+    error AuctionEndAlreadyCalled();
+
+    /// 使用 modifier 可以更便捷的校验函数的入参。
+    /// `onlyBefore` 会被用于后面的 `bid` 函数：
+    /// 新的函数体是由 modifier 本身的函数体，并用原函数体替换 `_;` 语句来组成的。
+    modifier onlyBefore(uint256 time) {
+        if (block.timestamp >= time) revert TooLate(time);
+        _;
+    }
+    modifier onlyAfter(uint256 time) {
+        if (block.timestamp <= time) revert TooEarly(time);
+        _;
+    }
+
+    constructor(
+        uint256 biddingTime,
+        uint256 revealTime,
+        address payable beneficiaryAddress
+    ) {
+        beneficiary = beneficiaryAddress;
+        biddingEnd = block.timestamp + biddingTime;
+        revealEnd = biddingEnd + revealTime;
+    }
+
+    /// 可以通过 `blindedBid` = keccak256(value, fake, secret)
+    /// 设置一个秘密竞拍。
+    /// 只有在出价披露阶段被正确披露，已发送的以太币才会被退还。
+    /// 如果与出价一起发送的以太币至少为 “value” 且 “fake” 不为真，则出价有效。
+    /// 将 “fake” 设置为 true ，然后发送满足订金金额但又不与出价相同的金额是隐藏实际出价的方法。
+    /// 同一个地址可以放置多个出价。
+    function bid(bytes32 blindedBid) external payable onlyBefore(biddingEnd) {
+        bids[msg.sender].push(
+            Bid({blindedBid: blindedBid, deposit: msg.value})
+        );
+    }
+
+    /// 披露你的秘密竞拍出价。
+    /// 对于所有正确披露的无效出价以及除最高出价以外的所有出价，你都将获得退款。
+    function reveal(
+        uint256[] calldata values,
+        bool[] calldata fake,
+        bytes32[] calldata secret
+    ) external onlyAfter(biddingEnd) onlyBefore(revealEnd) {
+        uint256 length = bids[msg.sender].length;
+        require(values.length == length);
+        require(fake.length == length);
+        require(secret.length == length);
+
+        uint256 refund;
+        for (uint256 i = 0; i < length; i++) {
+            Bid storage bid = bids[msg.sender][i];
+            (uint256 value, bool fake, bytes32 secret) = (
+                values[i],
+                fake[i],
+                secret[i]
+            );
+            if (bid.blindedBid != keccak256(value, fake, secret)) {
+                // 出价未能正确披露
+                // 不返还订金
+                continue;
+            }
+            refund += bid.deposit;
+            if (!fake && bid.deposit >= value) {
+                if (placeBid(msg.sender, value)) refund -= value;
+            }
+            // 使发送者不可能再次认领同一笔订金
+            bid.blindedBid = bytes32(0);
+        }
+        msg.sender.transfer(refund);
+    }
+
+    // 这是一个 "internal" 函数， 意味着它只能在本合约（或继承合约）内被调用
+    function placeBid(address bidder, uint256 value)
+        internal
+        returns (bool success)
+    {
+        if (value <= highestBid) {
+            return false;
+        }
+        if (highestBidder != address(0)) {
+            // 返还之前的最高出价
+            pendingReturns[highestBidder] += highestBid;
+        }
+        highestBid = value;
+        highestBidder = bidder;
+        return true;
+    }
+
+    /// 取回出价（当该出价已被超越）
+    function withdraw() external {
+        uint256 amount = pendingReturns[msg.sender];
+        if (amount > 0) {
+            // 这里很重要，首先要设零值。
+            // 因为，作为接收调用的一部分，
+            // 接收者可以在 `transfer` 返回之前重新调用该函数。（可查看上面关于‘条件 -> 影响 -> 交互’的标注）
+            pendingReturns[msg.sender] = 0;
+
+            msg.sender.transfer(amount);
+        }
+    }
+
+    /// 结束拍卖，并把最高的出价发送给受益人
+    function auctionEnd() external onlyAfter(revealEnd) {
+        if (ended) revert AuctionEndAlreadyCalled();
+        emit AuctionEnded(highestBidder, highestBid);
+        ended = true;
+        beneficiary.transfer(highestBid);
+    }
+}
