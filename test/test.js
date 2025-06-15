f0f15268dbd27648cf7d438b703ece94e0a157d5
zyronon <zyronon@163.com>
2025-06-15 17:16:13 +0800

    save

--- test/test.js
+++ test/test.js
@@ -1,25 +1,206 @@
-function cc(val) {
-  console.log(JSON.stringify(val));
+// ES6 ES2015
+// https://promisesaplus.com/
+const PROMISE_STATUS_PENDING = 'pending'
+const PROMISE_STATUS_FULFILLED = 'fulfilled'
+const PROMISE_STATUS_REJECTED = 'rejected'
+
+// 工具函数
+function execFunctionWithCatchError(execFn, value, resolve, reject) {
+  try {
+    const result = execFn(value)
+    resolve(result)
+  } catch (err) {
+    reject(err)
+  }
 }
 
-var checkPossibility = function (nums) {
-  const n = nums.length;
-  let cnt = 0;
-  for (let i = 0; i < n - 1; ++i) {
-    const x = nums[i],
-      y = nums[i + 1];
-    if (x > y) {
-      cnt++;
-      if (cnt > 1) {
-        return false;
-      }
-      if (i > 0 && y < nums[i - 1]) {
-        nums[i + 1] = x;
+class MyPromise {
+  constructor(executor) {
+    this.status = PROMISE_STATUS_PENDING
+    this.value = undefined
+    this.reason = undefined
+    this.onFulfilledFns = []
+    this.onRejectedFns = []
+
+    const resolve = (value) => {
+      if (this.status === PROMISE_STATUS_PENDING) {
+        // 添加微任务
+        queueMicrotask(() => {
+          if (this.status !== PROMISE_STATUS_PENDING) return
+          this.status = PROMISE_STATUS_FULFILLED
+          this.value = value
+          this.onFulfilledFns.forEach(fn => {
+            fn(this.value)
+          })
+        });
       }
     }
-  }
-  return true;
-};
 
-console.log(checkPossibility([2, 2, 1, 3, 3]));
-// console.log(findErrorNums([1, 1]));
+    const reject = (reason) => {
+      if (this.status === PROMISE_STATUS_PENDING) {
+        // 添加微任务
+        queueMicrotask(() => {
+          if (this.status !== PROMISE_STATUS_PENDING) return
+          this.status = PROMISE_STATUS_REJECTED
+          this.reason = reason
+          this.onRejectedFns.forEach(fn => {
+            fn(this.reason)
+          })
+        })
+      }
+    }
+
+    try {
+      executor(resolve, reject)
+    } catch (err) {
+      reject(err)
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    const defaultOnRejected = err => {
+      throw err
+    }
+    onRejected = onRejected || defaultOnRejected
+
+    const defaultOnFulfilled = value => {
+      return value
+    }
+    onFulfilled = onFulfilled || defaultOnFulfilled
+
+    return new MyPromise((resolve, reject) => {
+      // 1.如果在then调用的时候, 状态已经确定下来
+      if (this.status === PROMISE_STATUS_FULFILLED && onFulfilled) {
+        execFunctionWithCatchError(onFulfilled, this.value, resolve, reject)
+      }
+      if (this.status === PROMISE_STATUS_REJECTED && onRejected) {
+        execFunctionWithCatchError(onRejected, this.reason, resolve, reject)
+      }
+
+      // 2.将成功回调和失败的回调放到数组中
+      if (this.status === PROMISE_STATUS_PENDING) {
+        if (onFulfilled) this.onFulfilledFns.push(() => {
+          execFunctionWithCatchError(onFulfilled, this.value, resolve, reject)
+        })
+        if (onRejected) this.onRejectedFns.push(() => {
+          execFunctionWithCatchError(onRejected, this.reason, resolve, reject)
+        })
+      }
+    })
+  }
+
+  catch(onRejected) {
+    return this.then(undefined, onRejected)
+  }
+
+  finally(onFinally) {
+    this.then(() => {
+      onFinally()
+    }, () => {
+      onFinally()
+    })
+  }
+
+  static resolve(value) {
+    return new MyPromise((resolve) => resolve(value))
+  }
+
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => reject(reason))
+  }
+
+  static all(promises) {
+    // 问题关键: 什么时候要执行resolve, 什么时候要执行reject
+    return new MyPromise((resolve, reject) => {
+      const values = []
+      promises.forEach(promise => {
+        promise.then(res => {
+          values.push(res)
+          if (values.length === promises.length) {
+            resolve(values)
+          }
+        }, err => {
+          reject(err)
+        })
+      })
+    })
+  }
+
+  static allSettled(promises) {
+    return new MyPromise((resolve) => {
+      const results = []
+      promises.forEach(promise => {
+        promise.then(res => {
+          results.push({status: PROMISE_STATUS_FULFILLED, value: res})
+          if (results.length === promises.length) {
+            resolve(results)
+          }
+        }, err => {
+          results.push({status: PROMISE_STATUS_REJECTED, value: err})
+          if (results.length === promises.length) {
+            resolve(results)
+          }
+        })
+      })
+    })
+  }
+
+  static race(promises) {
+    return new MyPromise((resolve, reject) => {
+      promises.forEach(promise => {
+        promise.then(resolve, reject)
+      })
+    })
+  }
+
+  static any(promises) {
+    // resolve必须等到有一个成功的结果
+    // reject所有的都失败才执行reject
+    const reasons = []
+    return new MyPromise((resolve, reject) => {
+      promises.forEach(promise => {
+        promise.then(resolve, err => {
+          reasons.push(err)
+          if (reasons.length === promises.length) {
+            reject(new AggregateError(reasons))
+          }
+        })
+      })
+    })
+  }
+}
+
+
+//module.exports = MyPromise 导出模块用于后续测试
+
+// Promise.resolve().then(() => {
+//   console.log(0);
+//   return Promise.resolve(4);
+// }).then((res) => {
+//   console.log(res)
+// })
+
+let promise = new MyPromise((resolve, reject) => {
+  resolve(0)
+})
+promise.then(() => {
+  console.log(1);
+  // return 22
+  throw new Error("err message")
+
+}, err => {
+  console.log('err1 message', err)
+}).then((r) => {
+  console.log(2, r);
+}, err => {
+  console.log('err2 message', err)
+}).then(() => {
+  console.log(3);
+}).then(() => {
+  console.log(5);
+}).then(() => {
+  console.log(6);
+})
+// .catch(err => {
+// console.log('catch1', err);
+// })
--- test/test.ts
+++ test/test.ts
@@ -1,14 +1,63 @@
-function findErrorNums(nums) {
-  let map = new Map();
-  nums.map((v) => {
-    map.set(v, (map.get(v) || 0) + 1);
-  });
-  let max = 0;
-  let num = 0;
+const arr = [5, 3, 2, 7, 8, 34, 7, 39, 12, 56, 9, 1, 6]
 
-  console.log(Array.from(map));
+function bubbleSort(arr: Array<number>) {
+  const len = arr.length
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]
+      }
+    }
+  }
+  return arr
 }
 
-console.log(findErrorNums([1, 5, 3, 4, 5]));
-console.log(findErrorNums([1, 1]));
-console.log(findErrorNums([2, 2]));
+function quickSortInPlace(arr, left = 0, right = arr.length - 1) {
+  if (left >= right) return;
+
+  const pivotIndex = partition(arr, left, right);
+  console.log(pivotIndex)
+  // quickSortInPlace(arr, left, pivotIndex - 1);
+  // quickSortInPlace(arr, pivotIndex + 1, right);
+
+  return arr
+}
+
+function partition(arr, left, right) {
+  const pivot = arr[right];
+  let i = left;
+
+  for (let j = left; j < right; j++) {
+    if (arr[j] < pivot) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
+    }
+  }
+
+  [arr[i], arr[right]] = [arr[right], arr[i]];
+
+  return i;
+}
+
+function sss(arr, left = 0, right = arr.length - 1) {
+  if (left >= right) return;
+  let p = b(arr, left, right);
+  sss(arr, left, p - 1);
+  sss(arr, p + 1, right);
+  return arr
+}
+
+function b(arr, left, right) {
+  let p = arr[right];
+  let i = left;
+  for (let j = left; j < right; j++) {
+    if (arr[j] < p) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++
+    }
+  }
+  [arr[i], arr[right]] = [arr[right], arr[i]];
+  return i
+}
+
+console.log(sss(arr))
